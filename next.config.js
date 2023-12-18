88223cba9b4df77fc4cf6b8a85aaef46d4332653
wshi01 <jayceengineer2@gmail.com>
2023-12-18 15:26:26 -0500

    test

--- next.config.js
+++ next.config.js
@@ -4,8 +4,11 @@ const withNextra = require('nextra')({
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 })
 
+const isProd = process.env.NODE_ENV = 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: isProd ? '/nextjs-blog-deployment': '',
   output: 'export',
   distDir: 'dist',
   images: {
