a5b238cfd0cf6fe0c1e8eee4b635ea32a5210847
wshi01 <jayceengineer2@gmail.com>
2023-12-18 15:16:37 -0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.js',
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  }
+}
+
+module.exports = withNextra(nextConfig)
